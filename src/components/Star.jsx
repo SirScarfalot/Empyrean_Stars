644bbad4666b7605907f56fe8fc89cd6a00d93cd
Anton Duchaineau <Sir.Scarfalot2991@gmail.com>
2025-10-26 17:43:27 -0700

    actually import GetStarById

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -4,6 +4,7 @@ import { React, useState, useEffect, Suspense } from "react";
 import dynamic from "next/dynamic";
 import { getStarSnapshotById } from "@/src/lib/firebase/firestore.js";
 import StarDetails from "./StarDetails";
+import { getStarById } from "@/src/lib/firebase/firestore.js";
 import { useUser } from "@/src/lib/getUser";
 
 // const WarDialog = dynamic(() => import("@/src/components/WarDialog.jsx"));
